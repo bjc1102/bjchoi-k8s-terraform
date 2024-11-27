f0c1611130a9282a114a4e3e9dcbeea0c35d7dbf
bjc1102 <bjc30849@gmail.com>
2024-11-27 15:12:09 +0900

    Feat: Helm added

--- master/master.tf
+++ master/master.tf
@@ -104,6 +104,13 @@ chmod +x /etc/profile.d/k8s-completion.sh
 
 source /etc/profile.d/k8s-completion.sh
 
+# helm 설치
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
 EOT
 
   root_block_device {
